37215db1f8b726246e5ea26b8643bff6f8b24137
Michael Moffet <nanomoffet@gmail.com>
2024-01-21 14:21:32 -0800

    rev 7

--- keyboards/planck/rev7/keymaps/nanomoffet/keymap.c
+++ keyboards/planck/rev7/keymaps/nanomoffet/keymap.c
@@ -29,6 +29,7 @@ enum {
     CT_CBR,
     CT_PAR,
     CT_LN,
+	CT_TAB,
 };
 
 void dance_min_finished (tap_dance_state_t *state, void *user_data) {
@@ -125,35 +126,54 @@ void dance_ln_reset (tap_dance_state_t *state, void *user_data) {
   }
 }
 
+void dance_tab_finished (tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    register_code (KC_TAB);
+  } else {
+    register_code (KC_RSFT);
+    register_code (KC_TAB);
+  }
+}
+
+void dance_tab_reset (tap_dance_state_t *state, void *user_data) {
+  if (state->count == 1) {
+    unregister_code (KC_TAB);
+  } else {
+    unregister_code (KC_RSFT);
+    unregister_code (KC_TAB);
+  }
+}
+
 tap_dance_action_t tap_dance_actions[] = {
  [CT_MIN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_min_finished, dance_min_reset),
  [CT_SQBR] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_sqbr_finished, dance_sqbr_reset),
  [CT_CBR] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_cbr_finished, dance_cbr_reset),
  [CT_PAR] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_par_finished, dance_par_reset),
- [CT_LN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_ln_finished, dance_ln_reset)
+ [CT_LN] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_ln_finished, dance_ln_reset),
+ [CT_TAB] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, dance_tab_finished, dance_tab_reset)
 };
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Base Layer
- * ,-----------------------------------------------------------------------------------.
- * |QK_GES|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  -_  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  =   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |SFT   |      |      |      |      |      |      |      |      |      |      |      |
- * |(TAB) |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  '   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |ALT   |      |      |      |      |      |      |      |      |      |      |      |
- * |(SPC) | Ctrl | Alt  | GUI  | MO(1)| Space|Enter |MO(2) |  ()  |  {}  |  []  |  ^$  |
- * `-----------------------------------------------------------------------------------'
+ * ,------------------------------------------------------------------------------------.
+ * |QK_GESC|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |-------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  -_   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  =   |
+ * |-------+------+------+------+------+------+------+------+------+------+------+------|
+ * |SFT    |      |      |      |      |      |      |      |      |      |      |      |
+ * |(TAB)  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  '   |
+ * |-------+------+------+------+------+------+------+------+------+------+------+------|
+ * |ALT    |      |      |      |      |      |      |      |      |      |      |      |
+ * |(SPC)  | Ctrl | Alt  | GUI  | MO(1)| Space|Enter |MO(2) |  ()  |  {}  |  []  |  ^$  |
+ * `------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     QK_GESC,       KC_Q,KC_W,    KC_E,    KC_R,       KC_T,  KC_Y,   KC_U,       KC_I,  KC_O,    KC_P,    KC_BSPC,
     TD(CT_MIN),       KC_A,    KC_S,    KC_D,    KC_F, KC_G,  KC_H,   KC_J, KC_K,  KC_L,    KC_SCLN, KC_EQL,
-    SFT_T(KC_TAB), KC_Z,    KC_X,    KC_C,       KC_V,  KC_B,   KC_N,       KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-    ALT_T(KC_SPC), KC_LCTL, KC_LALT, KC_LGUI,    MO(1), KC_SPC, KC_ENT,     MO(2), TD(CT_PAR), TD(CT_CBR), TD(CT_SQBR), TD(CT_LN)
+    KC_LSFT, KC_Z,    KC_X,    KC_C,       KC_V,  KC_B,   KC_N,       KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    TD(CT_TAB), KC_LCTL, KC_LALT, KC_LGUI,    MO(1), KC_SPC, KC_ENT,     MO(2), TD(CT_PAR), TD(CT_CBR), TD(CT_SQBR), TD(CT_LN)
 ),
 
 /* Numpadfn Layer
@@ -169,10 +189,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NUMPADFN] = LAYOUT_planck_grid(
- _______, KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, _______, KC_7,    KC_8, KC_9,    _______, _______,
- _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,  _______, _______, KC_4,    KC_5, KC_6,    _______, _______,
- _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  _______, _______, KC_1,    KC_2, KC_3,    _______, _______,
- _______, _______, _______, _______, _______, _______, _______, MO(3), KC_0, _______, _______, _______
+ _______, KC_1,   KC_2, KC_3, KC_4, KC_5, KC_6, _______, KC_7,    KC_8, KC_9,    _______,
+ _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_4,    KC_5, KC_6,    _______,
+ _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,  _______, _______, _______, KC_1,    KC_2, KC_3,    _______,
+ _______, KC_F1,   KC_F2,   KC_F3,   KC_F4, _______, _______, _______, MO(3), _______, KC_0, _______
 ),
 
 
@@ -455,4 +475,3 @@ bool dip_switch_update_user(uint8_t index, bool active) {
     }
     return true;
 }
-
